dce39883eacfd4167a6155d91c45bf55cef8d5da
joewatt40 <wat19030@byui.edu>
2020-05-14 19:47:34 -0700

    Changes for Amberlee

--- src/passwordverification/Main.java
+++ src/passwordverification/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello from Amberlee!");
+        System.out.println("Hi Amberlee, this is Joe");
     }
 }
